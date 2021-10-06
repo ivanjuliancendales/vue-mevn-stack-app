1e23c96ffea853cd6a1aa83ed03886ecfe8bef9d
ivanjuliancendales <ivanjuliancendales@hotmail.com>
2021-10-05 19:49:40 -0500

    Commit Inicial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- Nav bar -->
+    <nav
+      class="navbar navbar-dark bg-dark justify-content-between flex-nowrap flex-row"
+    >
+      <div class="container">
+        <a class="navbar-brand float-left">Sistema de Evaluación 360º</a>
+        <ul class="nav navbar-nav flex-row float-right">
+          <li class="nav-item col-md-5">
+            <router-link class="nav-link pr-3" to="/"
+              >Ingrese evaluación</router-link
+            >
+          </li>
+          <li class="nav-item col-md-5">
+            <router-link class="nav-link" to="/view">Visualice evaluaciones</router-link>
+          </li>
+        </ul>
+
+        <button class="btn btn-success navbar-btn">Contacto</button>
+
+      </div>
+    </nav>
+    <!-- Router view -->
+    <div class="container mt-5">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/CreateComponent.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+
+            <h3 class="text-center">¿Qué es la Evaluación 360º?</h3>
+            <br>
+            <p>
+            La Evaluación 360º permite evaluar al personal de una manera más completa y con una 
+            óptica general. Anteriormente un cargo era evaluado por un superior, pero esto no 
+            permite resultados objetivos y concluyentes.
+            </p>
+
+            <p>
+            El Sistema de Evaluación 360º logra mejorar esta labor, convirtiéndose en la 
+            herramienta que ayuda para el desarrollo del Talento Humano de su 
+            Empresa.
+            </p>
+            
+            <img src="../Sistema_de_evaluacion_360.jpg" 
+            width="541" height="434"
+            alt="Sistema_360º" >
+            
+            <br><br><br>
+
+            <h3 class="text-center">Crear Evaluación</h3>
+            <form @submit.prevent="handleSubmitForm">
+                <div class="form-group">
+                    <label>Nombre</label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="student.name"
+                        required
+                    />
+                </div>
+                
+                <div class="form-group">
+                    <label>Identificación</label>
+                    <input
+                        type="email"
+                        class="form-control"
+                        v-model="student.email"
+                        required
+                    />
+                </div>
+                
+                <div class="form-group">
+                    <label>Evaluación</label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="student.phone"
+                        required
+                    />
+                </div>
+
+                <div class="form-group">
+                    <button class="btn btn-danger btn-block">Crear</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            student: {
+                nombre: "",
+                identificacion: "",
+                evaluacion: "",
+            },
+        };
+    },
+    methods: {
+       handleSubmitForm() { 
+           let apiURL = "http://localhost:4000/api/create-student"; 
+           
+           axios 
+            .post(apiURL, this.student) 
+            .then(() => { 
+                this.$router.push("/view"); 
+                this.student = { 
+                    name: "", 
+                    email: "", 
+                    phone: "", 
+                }; 
+            })
+            .catch((error) => { 
+                console.log(error); 
+            }); 
+        }, 
+    }, 
+}; 
+</script>  
